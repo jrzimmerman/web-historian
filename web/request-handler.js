9d1adbaf9a6059d03509a10d051ba6773b1d442e
Justin Zimmerman <zimmy130@gmail.com>
2015-10-28 22:14:24 -0400

    serve index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var utils = require('./http-helpers');
 // require more modules/folders here!
 
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(req.method === 'GET') {
+    // req.url('/', function() { res.on('')});
+    if(req.url === '/') {
+      fs.readFile('./web/public/index.html', function(err, data){
+        if(err) throw err;
+        // console.log(data);
+        res.writeHead(200, utils.headers);
+        res.end(data);
+      });
+    }
+  }
 };
